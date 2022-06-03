c91a162cbe2c88de14e3c8c97f423db57a03e3f0
Wihhy <andre69tkachenko@gmail.com>
2022-06-03 18:40:12 +0300

    finally done with wall

--- lesson_003/07_wall.py
+++ lesson_003/07_wall.py
@@ -1,36 +1,44 @@
 # -*- coding: utf-8 -*-
 
 # (цикл for)
-import simple_draw
+import simple_draw as sd
 
 # Нарисовать стену из кирпичей. Размер кирпича - 100х50
 # Использовать вложенные циклы for
+sd.resolution = (1200, 800)
+start_x = 0
+start_y = 0
+end_x = 100
+end_y = 50
+# i = 1
+# j = 1
+for j in range(12):
+    start_point = sd.get_point(x=0, y=start_y)
+    end_point = sd.get_point(x=600, y=start_y)
+    sd.line(start_point, end_point, color=sd.COLOR_YELLOW, width=2)
+    start_y += 50
+
+    for i in range(6):
+        # break
+        vertical_start_point = sd.get_point(x=start_x + 100, y=start_y + 50)
+        vertical_end_point = sd.get_point(x=start_x + 100, y=start_y)
+        sd.line(start_point=vertical_start_point, end_point=vertical_end_point, color=sd.COLOR_YELLOW, width=2)
+        start_x += 100
+
+        for k in range(6):
+            vertical_start_point2 = sd.get_point(x=start_x - 50, y=start_y)
+            vertical_end_point2 = sd.get_point(x=start_x - 50, y=start_y - 50)
+            sd.line(start_point=vertical_start_point2, end_point=vertical_end_point2, color=sd.COLOR_YELLOW, width=2)
+            start_x += 100
+        start_x = start_x - 600
+    start_x = start_x - 650
 
 
-start_x = simple_draw.get_point(0, 0)
-start_y = simple_draw.get_point(100, 50)
-i = 1
-bricks_positions_x = (
-    (simple_draw.get_point(0, 0)),
-    {0: 100},
-    {0: 200},
-    {0: 300},
-    {0: 400},
-    {0: 500},
-    {0: 600}
-)
-bricks_positions_y = (
-    (simple_draw.get_point(100, 50)),
-    {200: 50},
-    {300: 50},
-    {400: 50},
-    {500: 50},
-    {600: 50},
-    {700: 50}
-)
-for x in bricks_positions_x:
-    for y in bricks_positions_y:
-        left_bottom = bricks_positions_x[x]
 
-        simple_draw.rectangle(x, y)
-simple_draw.pause()
+
+
+sd.pause()
+
+
+
+
