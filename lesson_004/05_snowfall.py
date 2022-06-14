bd5c261a6d26ca42a7060fc089179a91ce4afcaf
Wihhy <andre69tkachenko@gmail.com>
2022-06-14 17:51:39 +0300

    snowfall not working

--- lesson_004/05_snowfall.py
+++ lesson_004/05_snowfall.py
@@ -11,7 +11,6 @@ sd.resolution = (1200, 600)
 # - нарисовать падение этих N снежинок
 # - создать список рандомных длинн лучей снежинок (от 10 до 100) и пусть все снежинки будут разные
 
-N = 20
 
 # Пригодятся функции
 # sd.get_point()
@@ -20,33 +19,41 @@ N = 20
 # sd.random_number()
 # sd.user_want_exit()
 
+N = 20
+
 y_list = [500, 490, 480, 400, 440]
 x_list = [100, 110, 120, 130]
-length_list = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100]
+length_list = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 15, 25, 35, 45, 55, 65, 75, 85, 95]
 print()
 
-y = 500
-x = 100
+y = random.choice(y_list)
+x = random.choice(x_list)
 
-y2 = 450
-x2 = 150
+y2 = random.choice(y_list)
+x2 = random.choice(x_list)
+
+
+length1 = random.choice(length_list)
+length2 = random.choice(length_list)
+
+
 while True:
     sd.clear_screen()
     point = sd.get_point(x, y)
-    sd.snowflake(center=point, length=50)
+    sd.snowflake(center=point, length=length1)
     y -= 10
     if y < 50:
         break
-    x = x + 10
+    x = x * round(random.uniform(1.15, 0.95), 2)
 
-    point2 = sd.get_point(x, y)
-    sd.snowflake(center=point2, length=70)
-    y -= 10
-    if y < 50:
+    point2 = sd.get_point(x2, y2)
+    sd.snowflake(center=point2, length=length2)
+    y2 -= 10
+    if y2 < 50:
         break
-    x2 = x2 + 20
+    x2 = x2 * round(random.uniform(1.15, 0.95), 2)
 
-    sd.sleep(0.1)
+    sd.sleep(0.07)
     if sd.user_want_exit():
         break
 
