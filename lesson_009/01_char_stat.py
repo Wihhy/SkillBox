dd9a938a84708e2e25c407b5196b2bbd198eae51
Wihhy <andre69tkachenko@gmail.com>
2022-07-02 22:24:28 +0300

    redone in classes

--- lesson_009/01_char_stat.py
+++ lesson_009/01_char_stat.py
@@ -20,20 +20,47 @@
 #
 # Упорядочивание по частоте - по убыванию. Ширину таблицы подберите по своему вкусу
 # Требования к коду: он должен быть готовым к расширению функциональности. Делать сразу на классах.
-from pprint import pprint
+import zipfile
 
-stat = {}
-char = 'а'
-with open('voyna-i-mir.txt', mode='r', encoding='cp1251') as text:
-    for line in text:
-        for char in line:
-            if char.isalpha():
-                if char in stat:
-                    stat[char] += 1
-                else:
-                    stat[char] = 1
 
-pprint(stat)
+class CharCounter:
+
+    def __init__(self, file_name):
+        self.stat = {}
+        self.file_name = file_name
+
+    def unzip(self):
+        zfile = zipfile.ZipFile(self.file_name, 'r')
+        for filename in zfile.namelist():
+            zfile.extract(filename)
+        self.file_name = filename
+
+    def count(self):
+        char_totals = 0
+        with open('voyna-i-mir.txt', mode='r', encoding='cp1251') as text:
+            for line in text:
+                for char in line:
+                    if char.isalpha():
+                        if char in self.stat:
+                            self.stat[char] += 1
+                        else:
+                            self.stat[char] = 1
+        # pprint(self.stat)
+        print(f'+{"-" * 9}+{"-" * 9}+''\n'
+              f'|  буква  | частота |''\n'
+              f'+{"-" * 9}+{"-" * 9}+')
+        for item in self.stat:
+            print(f'|{item:^9}|{self.stat[item]:^9}|')
+            char_totals += self.stat[item]
+        print(f'+{"-" * 9}+{"-" * 9}+''\n'
+              f'|  итого  |{char_totals:^9}|''\n'
+              f'+{"-" * 9}+{"-" * 9}+''\n')
+
+
+char_stat = CharCounter(file_name='E:\\PyCharm Projects\\course\\lesson_009\\python_snippets\\voyna-i-mir.txt.zip')
+char_stat.unzip()
+char_stat.count()
+
 
 # После выполнения первого этапа нужно сделать упорядочивание статистики
 #  - по частоте по возрастанию
