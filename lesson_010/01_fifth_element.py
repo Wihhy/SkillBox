af0b7aa735d155c3cf14f40d4696886e21b6c393
Wihhy <andre69tkachenko@gmail.com>
2022-07-12 19:50:25 +0300

    task 1 done

--- lesson_010/01_fifth_element.py
+++ lesson_010/01_fifth_element.py
@@ -5,16 +5,21 @@
 BRUCE_WILLIS = 42
 
 input_data = input('Если хочешь что-нибудь сделать, сделай это сам: ')
-leeloo = int(input_data[4])
-result = BRUCE_WILLIS * leeloo
-print(f"- Leeloo Dallas! Multi-pass № {result}!")
+try:
+    leeloo = int(input_data[4])
+    result = BRUCE_WILLIS * leeloo
+    print(f"- Leeloo Dallas! Multi-pass № {result}!")
+except ValueError as exc:
+    print(f'Введённое сообщение невозможно преобразовать в целое число, {exc}')
+except IndexError as exc:
+    print(f'Введено недостаточно символов!, {exc}')
+except Exception as exc:
+    print(f'Что то совсем непонятное: {exc}')
+
 
 # Ообернуть код и обработать исключительные ситуации для произвольных входных параметров
 # - ValueError - невозможно преобразовать к числу
 # - IndexError - выход за границы списка
 # - остальные исключения
 # для каждого типа исключений написать на консоль соотв. сообщение
-
-
-
 
